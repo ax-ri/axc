322e30c07d54194df263c7668d490011826f6c91
Axel RICHARD <axel.richard@ensta-paris.fr>
2025-05-16 11:54:35 +0200

    feat: first version of syntax

--- axcast.ml
+++ axcast.ml
@@ -1,19 +1,31 @@
+type pitch = 
+  | ESimplePitch of string                    (* one pitch (e.g. c4, a7 etc. )*)
+  | EMultiplePitch of (string list)           (* chord(<pitch> <pitch>...) *)
+  
+type rhythm =
+| ERhythm of int                          (* rhythm specifier *)
+
 type expr = 
-  | EIdent of string                          (* identifier *)
-  | EPitch of string                          (* pitch (e.g. c4, a7 etc. )*)
-  | ERhythm of (int)                          (* rhythm specifier *)
-  | ETempo of (expr * int)                    (* tempo(<rhythm> duration) *)
-  | EBeat of (int * expr)                     (* beat(count <rhythm>) *)
-  | ETie of (expr * expr * expr)              (* tie(<rhythm> <rhythm> <pitch>) *)
-  | EChord of (string list)                   (* chord(<pitch> <pitch>...) *)
+  | EIdent of string                      (* identifier *)
+  | ESound of (rhythm * pitch)            (* sound *)
+  | ETempo of (rhythm * int)              (* tempo(<rhythm> duration) *)
+  | EBeat of (int * rhythm)               (* beat(count <rhythm>) *)
+  | ETie of (rhythm * rhythm * pitch)     (* tie(<rhythm> <rhythm> <pitch>) *)
+;;
+
+let rec print_pitch oc = function
+  | ESimplePitch p -> Printf.fprintf oc "P%s" p
+  | EMultiplePitch l -> Printf.fprintf oc "Chord(%s)" (String.concat ", " l)
+;;
+;;
+let rec print_rhythm oc = function
+  | ERhythm i -> Printf.fprintf oc "R%d" i
 ;;
 
 let rec print oc = function
   | EIdent s -> Printf.fprintf oc "%s" s
-  | EPitch p -> Printf.fprintf oc "P%s" p
-  | ERhythm i -> Printf.fprintf oc "R%d" i
-  | ETempo(e, i) -> Printf.fprintf oc "Tempo %a = %d" print e i
-  | EBeat(i, e) -> Printf.fprintf oc "Beat %d x %a" i print e
-  | ETie(e1, e2, e3) -> Printf.fprintf oc "Tie %a %a %a" print e1 print e2 print e3
-  | EChord(l) -> Printf.fprintf oc "Chord(%s)" (String.concat ", " l)
-;;
+  | ESound(r, p) -> Printf.fprintf oc "Sound(%a %a)" print_rhythm r print_pitch p
+  | ETempo(r, i) -> Printf.fprintf oc "Tempo %a = %d" print_rhythm r i
+  | EBeat(i, r) -> Printf.fprintf oc "Beat %d x %a" i print_rhythm r
+  | ETie(r1, r2, p) -> Printf.fprintf oc "Tie %a %a %a" print_rhythm r1 print_rhythm r2 print_pitch p
+  
--- axcparse.mly
+++ axcparse.mly
@@ -19,16 +19,21 @@ main:
 ;
 
 expr: 
-    | PITCH                                         { EPitch($1) }
-    | IDENT                                         { EIdent($1) }
-    | RHYTHM INT                                    { ERhythm($2) }
-    | TEMPO expr INT                                { ETempo($2, $3) }
-    | BEAT INT expr                                 { EBeat($2, $3) }
-    | TIE LPAR expr expr expr RPAR                  { ETie($3, $4, $5) }
-    | DOT LPAR RHYTHM INT expr RPAR                 { ETie(ERhythm($4), ERhythm(2 * $4), $5) }
-    | CHORD LPAR pitch_list RPAR                    { EChord($3) }
+    | IDENT                                 { EIdent $1 }
+    | rhythm pitch                          { ESound($1, $2) }
+    | TEMPO rhythm INT                      { ETempo($2, $3) }
+    | BEAT INT rhythm                       { EBeat($2, $3) }
+    | TIE LPAR rhythm rhythm pitch RPAR     { ETie($3, $4, $5) }
+    | DOT LPAR RHYTHM INT pitch RPAR        { ETie(ERhythm($4), ERhythm(2 * $4), $5) }
+
+pitch:
+    | PITCH                         { ESimplePitch $1 }
+    | CHORD LPAR pitch_list RPAR    { EMultiplePitch $3 }
 
 pitch_list: 
- | { [] }
- | PITCH pitch_list { $1::$2 }
+    | { [] }
+    | PITCH pitch_list { $1::$2 }
+
+rhythm:
+    | RHYTHM INT    { ERhythm $2 }
 
